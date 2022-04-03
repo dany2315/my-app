38342d8b79b982d99f579f427d1f341953922509
Dany serfaty <davidserfaty2315@gmail.com>
2022-04-03 23:02:30 +0300

    initialisation

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Counters from './components/Counters';
+
+
+function App() {
+  return(
+    <>
+    <main className='main'>
+      <Counters/>
+    </main>
+    </>
+  )
+}
+
+
+
+export default App;
+
+
+
---
+++ src/components/Counter.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import bootstrap from '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            value:this.props.value
+        }
+    }
+  
+
+
+
+
+    handleIncrement = () => {
+        this.setState({ value: this.state.value + 1 });
+      };
+    
+    getBadgeClasses = () => {
+        let classes = "badge m-2 fs-6 bg-";
+        classes += this.state.value === 0 ? "warning" : "primary";
+        return classes;
+      };
+    
+    formatCount = () => {
+        const { value } = this.state;
+        return value === 0 ? "Zero" : value;
+      };
+    
+
+
+
+
+
+
+
+
+    render() { 
+        return ( 
+            <>
+            {this.state.loading ? (
+              <span>Loading... </span>
+            ) : (
+              <div className="counter">
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button
+                  onClick={this.handleIncrement}
+                  className="btn btn-secondary btn-sm"
+                >
+                  Increment
+                </button>
+              </div>
+            )}
+          </>
+         );
+    }
+}
+ 
+export default Counter;
